fb2b8d899b908ca85b52266a492fdec5ef4f154f
alon-ne <alonne@wix.com>
2017-01-30 15:49:27 +0200

    Refactored test_rtmp_listen
    divided main() code into separate functions

--- example/test_rtmp_listen.go
+++ example/test_rtmp_listen.go
@@ -23,13 +23,229 @@ const (
 	outputSampleRate = 48000
 )
 
-func checkAvRet(funcName string, avfunc func() int) bool {
-	if errnum := avfunc(); errnum < 0 {
-		errorMessage := avutil.AvStrError(errnum)
-		fmt.Printf("%s() failed: %s", funcName, errorMessage)
-		return false
+func initializeLog() {
+	avlog.AvlogStartLoggingToFile("/home/alon/test_rtmp_listen.log")
+	avlog.AvlogSetLevel(avlog.AV_LOG_DEBUG)
+}
+
+func createInputFormatContext() (*avformat.Context, error) {
+	avformat.AvformatNetworkInit()
+	var inputFormatContext *avformat.Context
+	var opts *avutil.Dictionary
+	avutil.AvDictSet(&opts, "listen", "1", 0)
+	uri := "rtmp://127.0.0.1:1935/app/stream"
+	fmt.Printf("Test rtmp server listening...\n")
+	if errNum := avformat.AvformatOpenInput(&inputFormatContext, uri, nil, &opts); errNum < 0 {
+		errorMessage := avutil.AvStrError(errNum)
+		return nil, errors.New("AvformatOpenInput() failed: " + errorMessage)
 	}
-	return true
+
+	fmt.Printf("AvformatOpenInput() returned ok\n")
+	fmt.Printf("Finding stream info\n")
+	if errNum := inputFormatContext.AvformatFindStreamInfo(nil); errNum < 0 {
+		errorMessage := avutil.AvStrError(errNum)
+		return nil, errors.New("AvformatFindStreamInfo() failed: " + errorMessage)
+	}
+	fmt.Printf("Dumping input format\n")
+	inputFormatContext.AvDumpFormat(0, "", 0)
+	return inputFormatContext, nil
+}
+
+func createOutputFormatContext() (*avformat.Context, error) {
+	avcodec.AvcodecRegisterAll()
+	avformat.AvRegisterAll()
+	outputFormat := avformat.AvGuessFormat(outputFormatName, "", "")
+	if outputFormat == nil {
+		return nil, errors.New("Failed to guess format for output format " + outputFormatName)
+	}
+
+	outputFormatContext := avformat.AvformatAllocContext()
+	if outputFormatContext == nil {
+		return nil, errors.New("Failed to allocate output format context")
+	}
+
+	return outputFormatContext, nil
+}
+
+func createInputStreams(inputFormatContext *avformat.Context) ([]*avformat.Stream, []*avcodec.Context, error) {
+	inputStreams := inputFormatContext.Streams()
+	numInputStreams := len(inputStreams)
+	inputCodecs := make([]*avcodec.Codec, numInputStreams)
+	inputCodecContexts := make([]*avcodec.Context, len(inputStreams))
+	for streamIndex, inputStream := range inputStreams {
+		inputCodecParams := inputStream.CodecPar()
+		inputStream.CodecPar()
+		inputCodecId := inputCodecParams.CodecId()
+		codec := avcodec.AvcodecFindDecoder(inputCodecId)
+		if codec == nil {
+			errorMessage := fmt.Sprintf("Failed to find decoder for codec id %d", inputCodecId)
+			return nil, nil, errors.New(errorMessage)
+		}
+		inputCodecContext := codec.AvcodecAllocContext3()
+		if inputCodecContext == nil {
+			return nil, nil, errors.New("Failed to allocate codec context")
+		}
+
+		if error := avcodec.AvcodecParametersToContext(inputCodecContext, inputCodecParams); error < 0 {
+			errorMessage := avutil.AvStrError(error)
+			return nil, nil, errors.New("Failed to get avcodec parameters: " + errorMessage)
+		}
+
+		if error := inputCodecContext.AvcodecOpen2(codec, nil); error < 0 {
+			errorMessage := avutil.AvStrError(error)
+			return nil, nil, errors.New("Failed to open input codec: " + errorMessage)
+		}
+
+		inputCodecs[streamIndex] = codec
+		inputCodecContexts[streamIndex] = inputCodecContext
+	}
+	fmt.Printf("Number of streams: %d\n", inputFormatContext.NbStreams())
+
+	return inputStreams, inputCodecContexts, nil
+}
+
+func createOutputStreams(
+	inputStreams []*avformat.Stream,
+	inputCodecContexts []*avcodec.Context,
+	outputFormatContext *avformat.Context) ([]*avformat.Stream, []*avcodec.Context, error) {
+
+	numInputStreams := len(inputStreams)
+	outputStreams := make([]*avformat.Stream, numInputStreams)
+	outputCodecContexts := make([]*avcodec.Context, numInputStreams)
+	for streamIndex, inputStream := range inputStreams {
+		inputCodecContext := inputCodecContexts[streamIndex]
+		inputCodecParams := inputStream.CodecPar()
+		outputStream, outputCodecContext, error := createOutputStream(inputCodecParams, inputCodecContext, outputFormatContext)
+		if error != nil {
+			errorMessage := fmt.Sprintf("Failed to create output stream %d: %v", streamIndex, error)
+			return nil, nil, errors.New(errorMessage)
+		}
+		outputStreams[streamIndex] = outputStream
+		outputCodecContexts[streamIndex] = outputCodecContext
+	}
+	return outputStreams, outputCodecContexts, nil
+}
+
+
+func readFrame(inputFormatContext *avformat.Context, inputPacket *avcodec.Packet) (gotCompleteFrame bool, haveMoreFrames bool, error error) {
+	gotCompleteFrame = true
+	haveMoreFrames = true
+	if errNum := inputFormatContext.AvReadFrame(inputPacket); errNum < 0 {
+		inputPacket.AvPacketUnref()
+		if errNum == avutil.AVERROR_EAGAIN {
+			gotCompleteFrame = false
+		} else if errNum == avutil.AVERROR_EOF {
+			haveMoreFrames = false
+		} else {
+			avErrorMessage := avutil.AvStrError(errNum)
+			errorMessage := fmt.Sprintf("Failed to read frame: %s (%d)\n", avErrorMessage, errNum)
+			error = errors.New(errorMessage)
+		}
+	}
+	return
+}
+
+func decodeFrame(
+	inputPacket *avcodec.Packet,
+	inputCodecContext *avcodec.Context,
+	decodedFrame *avutil.Frame) (gotCompleteFrame bool, error error) {
+
+	gotCompleteFrame = true
+	if errNum := inputCodecContext.AvCodecSendPacket(&inputPacket); errNum < 0 {
+		inputPacket.AvPacketUnref()
+		if errNum == avutil.AVERROR_EAGAIN {
+			gotCompleteFrame = false
+		} else {
+			avErrorMessage := avutil.AvStrError(errNum)
+			errorMessage := "Failed to send packet to input codec: " + avErrorMessage
+			error = errors.New(errorMessage)
+		}
+		return
+	}
+
+	if errNum := inputCodecContext.AvCodecReceiveFrame(decodedFrame); errNum < 0 {
+		inputPacket.AvPacketUnref()
+		if errNum == avutil.AVERROR_EAGAIN {
+			gotCompleteFrame = false
+		} else {
+			avErrorMessage := avutil.AvStrError(errNum)
+			errorMessage := "Failed to receive frame from input codec: " + avErrorMessage
+			error = errors.New(errorMessage)
+		}
+		return
+	}
+
+	timeStamp := avutil.AvFrameGetBestEffortTimestamp(decodedFrame)
+	decodedFrame.SetPts(timeStamp)
+	return
+}
+
+func encodeFrame(
+	outputCodecContext *avcodec.Context,
+	inputStream *avformat.Stream,
+	outputStream *avformat.Stream,
+	inputFrame *avutil.Frame) (*avcodec.Packet, *avutil.Error) {
+	if errNum := outputCodecContext.AvCodecSendFrame(inputFrame); errNum < 0 {
+		fmt.Printf("AvCodecSendFrame() failed with error %d\n", errNum)
+		return nil, &avutil.Error{errNum}
+	}
+	var encodedPacket avcodec.Packet
+	encodedPacket.AvInitPacket()
+	if errNum := outputCodecContext.AvCodecReceivePacket(&encodedPacket); errNum < 0 {
+		fmt.Printf("AvCodecReceivePacket() failed with error %d\n", errNum)
+		return nil, &avutil.Error{errNum}
+	}
+
+	presentationTimeStamp := inputFrame.Pts()
+	decompressionTimeStamp := inputFrame.Pts()
+	streamIndex := inputStream.Index()
+	inputTimeBase := inputStream.TimeBase()
+	outputTimeBase := outputStream.TimeBase()
+	encodedPacket.SetStreamIndex(streamIndex)
+	encodedPacket.SetPts(presentationTimeStamp)
+	encodedPacket.SetDts(decompressionTimeStamp)
+	encodedPacket.AvPacketRescaleTs(inputTimeBase, outputTimeBase)
+
+	return &encodedPacket, nil
+}
+
+func createOutputStream(
+	inputCodecParams *avcodec.CodecParameters,
+	inputCodecContext *avcodec.Context,
+	outputFormatContext *avformat.Context) (*avformat.Stream, *avcodec.Context, error) {
+
+	codecType := inputCodecParams.CodecType()
+	var outputStream *avformat.Stream
+	var outputCodecContext *avcodec.Context
+	var error error
+	switch codecType {
+	case avutil.AVMEDIA_TYPE_AUDIO:
+		width := inputCodecContext.Width()
+		height := inputCodecContext.Height()
+		outputStream, outputCodecContext, error = createVideoOutputStream(
+			width,
+			height,
+			outputVideoCodecId,
+			outputFormatContext)
+
+	case avutil.AVMEDIA_TYPE_VIDEO:
+		outputStream, outputCodecContext, error = createAudioOutputStream(
+			outputSampleRate,
+			outputAudioCodecId,
+			outputFormatContext)
+
+	default:
+		errorMessage := fmt.Sprintf("Unknown codec type %d", codecType)
+		return nil, nil, errors.New(errorMessage)
+
+	}
+
+	if error != nil {
+		errorMessage := fmt.Sprintf("Failed to create output stream: %v", error)
+		return nil, nil, errors.New(errorMessage)
+	}
+
+	return outputStream, outputCodecContext, nil
 }
 
 func createVideoOutputStream(width, height int, outputCodecId int, outputFormatContext *avformat.Context) (*avformat.Stream, *avcodec.Context, error) {
@@ -118,220 +334,84 @@ func createAudioOutputStream(sampleRate int, outputCodecId int, outputFormatCont
 	}
 
 	//TODO: Try passing outputCodec here, see if that works
-	if errnum := outputCodecContext.AvcodecOpen2(nil, nil); errnum < 0 {
-		fmt.Printf("AvcodecOpen2() failed with error %d\n", errnum)
-		return nil, nil, &avutil.Error{errnum}
+	if errNum := outputCodecContext.AvcodecOpen2(nil, nil); errNum < 0 {
+		fmt.Printf("AvcodecOpen2() failed with error %d\n", errNum)
+		return nil, nil, &avutil.Error{errNum}
 	}
 
 	// copy the stream parameters to the output stream
-	if errnum := avcodec.AvcodecParametersFromContext(outputStream.CodecPar(), outputCodecContext); errnum < 0 {
-		fmt.Printf("AvcodecParametersFromContext() failed with error %d\n", errnum)
-		return nil, nil, &avutil.Error{errnum}
+	if errNum := avcodec.AvcodecParametersFromContext(outputStream.CodecPar(), outputCodecContext); errNum < 0 {
+		fmt.Printf("AvcodecParametersFromContext() failed with error %d\n", errNum)
+		return nil, nil, &avutil.Error{errNum}
 	}
 
 
 	return outputStream, outputCodecContext, nil
 }
 
-func encodeFrame(outputCodecContext *avcodec.Context, inputFrame *avutil.Frame) (*avcodec.Packet, *avutil.Error) {
-	if errnum := outputCodecContext.AvCodecSendFrame(inputFrame); errnum < 0 {
-		fmt.Printf("AvCodecSendFrame() failed with error %d\n", errnum)
-		return nil, &avutil.Error{errnum}
-	}
-	var encodedPacket avcodec.Packet
-	encodedPacket.AvInitPacket()
-	if errnum := outputCodecContext.AvCodecReceivePacket(&encodedPacket); errnum < 0 {
-		fmt.Printf("AvCodecReceivePacket() failed with error %d\n", errnum)
-		return nil, &avutil.Error{errnum}
-	}
-	//Alon: Got this timestamp trick from ffmpeg muxing.c example
-	presentationTimeStamp := inputFrame.Pts()
-	decompressionTimeStamp := inputFrame.Pts()
-	encodedPacket.SetPts(presentationTimeStamp)
-	encodedPacket.SetDts(decompressionTimeStamp)
-	return &encodedPacket, nil
-}
-
-func GetOutputCodecId(codecType avcodec.MediaType) int {
-	outputAudioCodecId := avcodec.AV_CODEC_ID_AAC
-	outputVideoCodecId := avcodec.AV_CODEC_ID_H264
-
-	var outputCodecId int
-	switch codecType {
-	case avutil.AVMEDIA_TYPE_AUDIO:
-		outputCodecId = outputAudioCodecId
-	case avutil.AVMEDIA_TYPE_VIDEO:
-		outputCodecId = outputVideoCodecId
-	default:
-		outputCodecId = avcodec.AV_CODEC_ID_FIRST_UNKNOWN
-	}
-
-	return outputCodecId
-}
-
 func main() {
-	avlog.AvlogStartLoggingToFile("/home/alon/test_rtmp_listen.log")
-	avlog.AvlogSetLevel(avlog.AV_LOG_DEBUG);
+	initializeLog()
 
-	avformat.AvformatNetworkInit()
-	var inputFormatContext *avformat.Context
-	var opts *avutil.Dictionary
-	avutil.AvDictSet(&opts, "listen", "1", 0)
-	//TODO: Set live to 1 in opts maybe?
-
-	uri := "rtmp://127.0.0.1:1935/app/stream"
-	fmt.Printf("Test rtmp server listening...\n")
-	if errnum := avformat.AvformatOpenInput(&inputFormatContext, uri, nil, &opts); errnum < 0 {
-		errorMessage := avutil.AvStrError(errnum)
-		fmt.Printf("AvformatOpenInput() failed: %s\n", errorMessage)
+	inputFormatContext, error := createInputFormatContext()
+	if error != nil {
+		fmt.Printf("Failed to open input format context: %v\n", error)
 		return
 	}
 
-	fmt.Printf("AvformatOpenInput() returned ok\n")
-
-	fmt.Printf("Finding stream info\n")
-	if errnum := inputFormatContext.AvformatFindStreamInfo(nil); errnum < 0 {
-		errorMessage := avutil.AvStrError(errnum)
-		fmt.Printf("AvformatFindStreamInfo() failed: %s\n", errorMessage)
+	outputFormatContext, error := createOutputFormatContext()
+	if error != nil {
+		fmt.Printf("Failed to open output format context: %v\n", error)
 		return
 	}
 
-	fmt.Printf("Dumping input format\n")
-	inputFormatContext.AvDumpFormat(0, "", 0)
-
-	inputStreams := inputFormatContext.Streams()
-	numInputStreams := len(inputStreams)
-	inputCodecs := make([]*avcodec.Codec, numInputStreams)
-	inputCodecContexts := make([]*avcodec.Context, len(inputStreams))
-
-	/* Create output */
-	avcodec.AvcodecRegisterAll()
-	avformat.AvRegisterAll()
-	outputFormat := avformat.AvGuessFormat(outputFormatName, "", "")
-	if outputFormat == nil {
-		fmt.Printf("Failed to guess format for output format '%s'\n", outputFormatName)
-		return
-	}
-	outputFormatContext := avformat.AvformatAllocContext()
-	if outputFormatContext == nil {
-		fmt.Printf("Failed to allocate output format context\n")
+	inputStreams, inputCodecContexts, error := createInputStreams(inputFormatContext)
+	if error != nil {
+		fmt.Printf("Failed to create input streams: %v\n", error)
 		return
 	}
 
-	outputStreams := make([]*avformat.Stream, numInputStreams)
-	outputCodecContexts := make([]*avcodec.Context, numInputStreams)
-
-	/* Initialize Input and Output codecs and streams */
-	for streamIndex, inputStream := range inputStreams {
-		inputCodecParams := inputStream.CodecPar()
-		inputStream.CodecPar()
-		inputCodecId := inputCodecParams.CodecId()
-		codec := avcodec.AvcodecFindDecoder(inputCodecId)
-		if codec == nil {
-			fmt.Printf("Failed to find decoder for codec id %d\n", inputCodecId)
-			return
-		}
-		inputCodecContext := codec.AvcodecAllocContext3()
-		if inputCodecContext == nil {
-			fmt.Printf("Failed to allocate codec context\n")
-			return
-		}
-
-		if error := avcodec.AvcodecParametersToContext(inputCodecContext, inputCodecParams); error < 0 {
-			errorMessage := avutil.AvStrError(error)
-			fmt.Printf("Failed to get avcodec parameters: %s\n", errorMessage)
-			return
-		}
-
-		if error := inputCodecContext.AvcodecOpen2(codec, nil); error < 0 {
-			errorMessage := avutil.AvStrError(error)
-			fmt.Printf("Failed to open input codec: %s\n", errorMessage)
-			return
-		}
-
-		inputCodecs[streamIndex] = codec
-		inputCodecContexts[streamIndex] = inputCodecContext
-
-		codecType := inputCodecParams.CodecType()
-		var outputStream *avformat.Stream
-		var outputCodecContext *avcodec.Context
-		var error error
-
-		switch codecType {
-		case avutil.AVMEDIA_TYPE_AUDIO:
-			width := inputCodecContext.Width()
-			height := inputCodecContext.Height()
-			outputStream, outputCodecContext, error = createVideoOutputStream(
-				width,
-				height,
-				outputVideoCodecId,
-				outputFormatContext)
-
-		case avutil.AVMEDIA_TYPE_VIDEO:
-			outputStream, outputCodecContext, error = createAudioOutputStream(
-				outputSampleRate,
-				outputAudioCodecId,
-				outputFormatContext)
-
-		default:
-			fmt.Printf("Unknown codec type %d\n", codecType)
-			return
-		}
-
-		if error != nil {
-			fmt.Printf("Failed to create output stream: %v\n", error)
-			return
-		}
-		outputStreams[streamIndex] = outputStream
-		outputCodecContexts[streamIndex] = outputCodecContext
+	outputStreams, outputCodecContexts, error := createOutputStreams(inputStreams, inputCodecContexts, outputFormatContext)
+	if error != nil {
+		fmt.Printf("Failed to create output streams: %v\n", error)
+		return
 	}
 
-	fmt.Printf("Number of streams: %d\n", inputFormatContext.NbStreams())
 	var inputPacket avcodec.Packet
 	decodedFrame := avutil.AvFrameAlloc()
+	numInputStreams := len(inputStreams)
 	numFrames := make([]int, numInputStreams)
 
 	for {
-		/* Read Frame */
-		if error := inputFormatContext.AvReadFrame(&inputPacket); error < 0 {
-			inputPacket.AvPacketUnref()
-			if error == avutil.AVERROR_EAGAIN {
-				continue
-			} else if error == avutil.AVERROR_EOF {
-				fmt.Printf("Received EOF, stopping.")
-				break
-			}
+		gotCompleteFrame, haveMoreFrames, error := readFrame(inputFormatContext, &inputPacket)
 
-			errorMessage := avutil.AvStrError(error)
-			fmt.Printf("Failed to read frame: %s (%d)\n", errorMessage, error)
+		if error != nil {
+			fmt.Printf("Failed to read frame: %v", error)
+			return
+		}
+
+		if !haveMoreFrames {
 			break
 		}
 
-		/* Decode frame */
+		if !gotCompleteFrame {
+			continue
+		}
+
 		streamIndex := inputPacket.StreamIndex()
+		inputStream := inputStreams[streamIndex]
+		outputStream := outputStreams[streamIndex]
+
 		inputCodecContext := inputCodecContexts[streamIndex]
-		if error := inputCodecContext.AvCodecSendPacket(&inputPacket); error < 0 {
-			inputPacket.AvPacketUnref()
-			if error == avutil.AVERROR_EAGAIN {
-				continue
-			}
-			errorMessage := avutil.AvStrError(error)
-			fmt.Printf("Failed to send packet to input codec: %s\n", errorMessage)
-			break
+		gotCompleteFrame, error = decodeFrame(inputPacket, inputCodecContext, decodedFrame)
+		if error != nil {
+			fmt.Printf("Failed to decode frame: %v", error)
+			return
 		}
 
-		if error := inputCodecContext.AvCodecReceiveFrame(decodedFrame); error < 0 {
-			inputPacket.AvPacketUnref()
-			if error == avutil.AVERROR_EAGAIN {
-				continue
-			}
-			errorMessage := avutil.AvStrError(error)
-			fmt.Printf("Failed to receive frame from input codec: %s\n", errorMessage)
-			break
+		if !gotCompleteFrame {
+			continue
 		}
 
-		timeStamp := avutil.AvFrameGetBestEffortTimestamp(decodedFrame)
-		decodedFrame.SetPts(timeStamp)
 		streamNumFrames := &numFrames[streamIndex]
 		*streamNumFrames++
 		fmt.Printf("Stream %d: Received %d frames\n", streamIndex, *streamNumFrames)
@@ -339,29 +419,26 @@ func main() {
 		/* Encode Frame */
 		outputCodecContext := outputCodecContexts[streamIndex]
 		fmt.Printf("Encoding frame to stream %d\n", streamIndex)
-		encodedPacket, error := encodeFrame(outputCodecContext, decodedFrame)
+		encodedPacket, error := encodeFrame(outputCodecContext, inputStream, outputStream, decodedFrame)
 		if error != nil {
+			inputPacket.AvPacketUnref()
 			fmt.Printf("Failed to encode frame: %v\n", error)
 			return
 		}
-
-		inputStream := inputStreams[streamIndex]
-		outputStream := outputStreams[streamIndex]
-		encodedPacket.AvPacketRescaleTs(inputStream.TimeBase(), outputStream.TimeBase())
-		encodedPacket.SetStreamIndex(streamIndex)
 
 		/* Write frame */
 		if error := outputFormatContext.AvInterleavedWriteFrame(encodedPacket); error < 0 {
 			errorMessage := avutil.AvStrError(error)
 			fmt.Printf("Failed to write frame: %s\n", errorMessage)
+			inputPacket.AvPacketUnref()
 			encodedPacket.AvPacketUnref()
 			return
 		}
+
 		encodedPacket.AvPacketUnref()
 		inputPacket.AvPacketUnref()
 
 	}
 
 	//avutil.AvFrameFree(frame)
-
 }
